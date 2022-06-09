6a6054122b103a71c01d5dfd491c850899f40920
Sniponey <sniponey@gmail.com>
2022-06-09 14:59:41 +0200

    Ajout du formulaire de connexion

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -6,13 +6,15 @@ import * as yup from "yup";
 
 
 interface IFormInputs {
-    firstName: string
-    age: number
+    email: string
+    password: number
 }
   
 const schema = yup.object({
-    firstName: yup.string().required(),
-    age: yup.number().positive().integer().required(),
+    email: yup.string().email().required("Merci de renseigner votre email"),
+    password: yup.string().required("Merci de renseigner votre mot de passe").matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+    "Le mot de passe doit contenir au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial"),
+    /* confirmPassword: yup.string().required().oneOf([yup.ref("password"), null], "Les mots de passe doivent être identiques") */
 }).required();
 
 
@@ -25,16 +27,16 @@ const LoginForm: React.FC = () => {
     
       return (
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("firstName")} />
-          <p>{errors.firstName?.message}</p>
+          <input {...register("email")} placeholder="Email" type="email" required />
+          <p>{errors.email?.message}</p>
             
-          <input {...register("age")} />
-          <p>{errors.age?.message}</p>
+          <input {...register("password")} placeholder="Mot de passe" type="password" required />
+          <p>{errors.password?.message}</p>
           
           <input type="submit" />
         </form>
       );
-      
+
 };
 
 export default LoginForm;
