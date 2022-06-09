a29480792c96f818f0a4e6f65cb2e7bd38537c4c
Sniponey <sniponey@gmail.com>
2022-06-09 14:21:57 +0200

    J'ai changé des choses

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,33 +1,40 @@
 import './PageContainer.css';
 import React from 'react';
-import { useForm, SubmitHandler } from "react-hook-form";
-import { IonList, IonButton , IonIcon , IonItem, IonLabel, IonInput, IonToggle, IonRadio, IonCheckbox, IonItemSliding, IonItemOption, IonItemOptions, IonContent } from '@ionic/react';
-import { createOutline } from 'ionicons/icons';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
-type Inputs = {
-    example: string,
-    exampleRequired: string,
-  };
+
+interface IFormInputs {
+    firstName: string
+    age: number
+}
+  
+const schema = yup.object({
+    firstName: yup.string().required(),
+    age: yup.number().positive().integer().required(),
+}).required();
+
 
 const LoginForm: React.FC = () => {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
-
-    return (
-        /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
+    const { register, handleSubmit, formState: { errors } } = useForm<IFormInputs>({
+        resolver: yupResolver(schema)
+      });
+      const onSubmit = (data: IFormInputs) => console.log(data);
+    
+      return (
         <form onSubmit={handleSubmit(onSubmit)}>
-          {/* register your input into the hook by invoking the "register" function */}
-          <input defaultValue="test" {...register("example")} />
-          
-          {/* include validation with required or other standard HTML validation rules */}
-          <input {...register("exampleRequired", { required: true })} />
-          {/* errors will return when field validation fails  */}
-          {errors.exampleRequired && <span>This field is required</span>}
+          <input {...register("firstName")} />
+          <p>{errors.firstName?.message}</p>
+            
+          <input {...register("age")} />
+          <p>{errors.age?.message}</p>
           
           <input type="submit" />
         </form>
       );
+      
 };
 
 export default LoginForm;
