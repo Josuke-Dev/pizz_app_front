b2dc4c1bd08d5751da52107d7046d86d73b52d2b
viavie-dev <chaixvirginie883@gmail.com>
2022-06-23 20:02:11 +0200

    connexion api

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,25 +1,55 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api/";
+const API_URL = "https://localhost:8000/api/login_check";
 const config = {
   headers: {
     'Content-Type':'application/json',
     'Accept':'application/json',
-    "Access-Control-Allow-Origin": "*",
+   ' Access-Control-Allow-Origin': '*',
+
   },
 };
 
 class AuthService {
 
-  login(username, password) {
-    return axios
+  async login(username, password) {
+
+
+
+    const userObject = {
+      'username': username,
+      'password': password
+    };
+    console.log(userObject);
+    try {
+      const response = await axios.post(API_URL,  userObject,  {
+        headers: {
+          'Content-Type': 'application/json',
+
+          'Accept': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'X-Requested-With': 'XMLHttpRequest' // type de requete
+
+
+
+        }});
+
+      console.log(response.data);
+    } catch (error) {
+        console.log(error)
+    }
+
+
+    /*return axios
       .post(API_URL + "login_check", { username, password }, config)
       .then((response) => {
-        if (response.data.accessToken) {
+        console.log('login2');
+        console.log(response);
+      if (response.data.accessToken) {
           localStorage.setItem("user", JSON.stringify(response.data));
         }
         return response.data;
-      });
+      });*/
   }
 
   logout() {
